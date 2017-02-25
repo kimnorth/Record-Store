183a5547a066d8875e49fa3c13fa2ad38705b48e
kimnorth <benignhumour@gmail.com>
2017-02-25 16:15:16 +0000

    Added get and post request to add new album from page

--- ruby_controller.rb
+++ ruby_controller.rb
@@ -6,6 +6,20 @@ require_relative('./models/album.rb')
 
 get "/music-store" do
   @all_albums = Album.return_all_stock()
-  erb( :"index" )
+  erb( :index )
 end
 
+# get the form back to make a new album
+
+get "/music-store/new" do
+  # @album_array = Album.return_all_stock()
+  erb ( :new )
+end
+
+# submit new artist form
+
+post "/music-store" do
+  @album = Album.new(params)
+  @album.save()
+  redirect to( "/music-store" )
+end
