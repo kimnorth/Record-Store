09ebdbf9f4e438208c953445acb06e03e47551fc
kimnorth <benignhumour@gmail.com>
2017-03-01 11:18:09 +0000

    Added code that means you can type in characters with mixed capitalization and ruby will convert it to the right capitalization to search the database. I will need to change the information that gets entered in when you create an album with artist so that it matches this approach. The actual solution to make each word capitalized was something I found online, but I've included an explanation of what it's doing in the code.

--- controllers/search_controller.rb
+++ controllers/search_controller.rb
@@ -2,16 +2,24 @@ require('sinatra')
 require('sinatra/contrib/all')
 require_relative('../models/album.rb')
 
+# I freely admit that this following block of code isn't mine, but I do understand what it is doing! The String class doesn't implicitly have a titleize method, so this is creating a new method for the String class that splits out a phrase, capitalizes each word and joins them back together with a space. It's so I can search for an artist where a user types in lowercase, uppercase, etc.
+
+class String
+  def titleize
+    self.split(" ").map{|word| word.capitalize}.join(" ")
+  end
+end
+
 # Get all artists
 
 get '/search/search_albums' do
+  params["title"] = params["title"].downcase.titleize
   @found_albums = Album.find_by_name(params["title"])
   erb ( :"search/search_albums" )
 end
 
 get '/search/search_artists' do
-  puts params
+  params["artist"] = params["artist"].downcase.titleize
   @found_albums = Album.find_album_by_artist(params["artist"])
-  puts @found_albums
   erb (:"search/search_artists")
 end
